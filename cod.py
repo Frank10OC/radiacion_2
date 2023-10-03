1ed6975110610793cc3d1e2903a47b06d1e12e36
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-10-03 15:08:26 -0500

    Update cod.py

--- cod.py
+++ cod.py
@@ -8,6 +8,7 @@ st.title("Cálculo de ω y Actualización del CSV")
 phi = st.number_input("Ingrese el valor de ϕ:", min_value=0.0, max_value=90.0, step=0.1)
 # Leer el archivo CSV en un DataFrame
 df = pd.read_csv("https://raw.githubusercontent.com/Frank10OC/radiacion_2/main/dato.csv")
+st.write(df)
 st.write(df["Declinación Solar"])
 # Calcular ω utilizando la fórmula y agregarlo como una nueva columna
 df["Hora_Solar"] = np.arccos(-np.tan(np.radians(df["Declinación Solar"])) * np.tan(np.radians(phi)))
