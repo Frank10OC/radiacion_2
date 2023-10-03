c3b68e0d0585fc7f15bfe38dbafad3586401d2db
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-10-03 16:32:15 -0500

    Update cod.py

--- cod.py
+++ cod.py
@@ -5,21 +5,20 @@ import numpy as np
 def radianes_a_grados(radianes):
     grados = radianes * (180 / math.pi)
     return grados
+st.title("Cálculo de Radiación Solar en la Atmósfera Exterior")
 # Crear la aplicación Streamlit
-st.title("Cálculo de ω y Actualización del CSV")
-
+st.write("Cálculo de ángulo horario de salida del Sol")
 # Agregar un campo de entrada para ϕ
 phi = st.number_input("Ingrese la longitud (ϕ):", min_value=-180.0, max_value=180.0, step=0.000001)
 # Leer el archivo CSV en un DataFrame
 df = pd.read_csv("https://raw.githubusercontent.com/Frank10OC/Radiacion/main/dato.csv")
-st.write(df["Declinación Solar"])
 # Calcular ω utilizando la fórmula y agregarlo como una nueva columna
 df["Hora Solar"] = radianes_a_grados(np.arccos(-np.tan(np.radians(df["Declinación Solar"])) * np.tan(np.radians(phi))))
 # Actualizar la visualización del DataFrame con la nueva columna
 st.write(df)
 
 # Título de la aplicación
-st.title("Cálculo de Radiación Solar en la Atmósfera Exterior")
+
 # Conversión de grados a radianes
 omega =np.radians(df['Hora Solar']) 
 delta = np.radians(df['Declinación Solar'])
@@ -30,7 +29,6 @@ factor = (24 / math.pi) * df['Radiación Solar']
 term1 = (math.pi / 180) * radianes_a_grados(omega) * np.sin(delta) * np.sin(phi)
 term2 = np.cos(delta) * np.cos(phi) * np.sin(omega)
 ho = factor * (term1 + term2)
-st.write(omega)
 # Agregar los resultados al DataFrame
 df['Radiación Solar Exterior'] = ho
 # Mostrar el DataFrame con los resultados
