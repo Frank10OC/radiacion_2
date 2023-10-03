8f7d51fd5e9f00c8a4cd6f049b3e6d6b18ed18fb
Frank10OC <111895356+Frank10OC@users.noreply.github.com>
2023-10-03 14:49:33 -0500

    Update cod.py

--- cod.py
+++ cod.py
@@ -1,11 +1,29 @@
-phi = st.input("Ingrese el valor de ϕ:", min_value=0.0, max_value=90.0, step=0.1)
-# Verificar si se ha cargado un archivo CSV
-if uploaded_file is not None:
-    # Leer el archivo CSV en un DataFrame
-    df = pd.read_csv(uploaded_file)
+import streamlit as st
+import pandas as pd
+import math
 
-    # Calcular ω utilizando la fórmula y agregarlo como una nueva columna
-    df["Hora Solar"] = np.arccos(-np.tan(np.radians(df["δ"])) * np.tan(np.radians(phi)))
+# Función para calcular la radiación solar
+def calcular_radiacion_solar(N):
+    Ics = 4921.2  # Radiación solar extraterrestre en W/m^2
+    angulo_solar = 360 * N / 365
+    coseno_term = 0.0033 * math.cos(math.radians(angulo_solar))
+    radiacion_solar = Ics * (1 + coseno_term)
+    return radiacion_solar
 
-    # Actualizar la visualización del DataFrame con la nueva columna
-    st.write(df)
+# Crear un DataFrame llamado 'operaciones'
+operaciones = pd.DataFrame({'N': range(1, 366)})
+
+# Agregar una columna 'RadiacionSolar' para almacenar los resultados
+operaciones['RadiacionSolar'] = operaciones['N'].apply(calcular_radiacion_solar)
+
+# Configurar la aplicación Streamlit
+st.title('Operaciones con DataFrame')
+st.sidebar.header('Configuración')
+
+# Mostrar el DataFrame 'operaciones' en la interfaz
+st.write(operaciones)
+
+# Opcional: Guardar el DataFrame 'operaciones' en un archivo CSV
+if st.button('Guardar DataFrame'):
+    operaciones.to_csv('operaciones_radiacion_solar.csv', index=False)
+    st.success('DataFrame guardado en operaciones_radiacion_solar.csv')
